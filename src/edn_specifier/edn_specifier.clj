0a45815567f940c405e8c812ab6b5ccc977abf07
Sander Wiig <sander.wiig@student.uib.no>
2025-10-09 13:47:32 +0200

    project skeleton

---
+++ src/edn_specifier/edn_specifier.clj
@@ -0,0 +1,6 @@
+(ns edn-specifier.edn-specifier)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (prn x "Hello, World!"))
---
+++ test/edn_specifier/edn_specifier_test.clj
@@ -0,0 +1,7 @@
+(ns edn-specifier.edn-specifier-test
+  (:require [clojure.test :refer [deftest is testing]]
+            [edn-specifier.edn-specifier :as sut])) ; system under test
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
